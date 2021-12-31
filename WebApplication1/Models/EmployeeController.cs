4874502b712d2cea7744aa65342a137db82ab2be
205-ChalimamidiHemanthKumar <imhemanth5@gmail.com>
2021-12-31 02:07:08 -0800

    Add project files.

---
+++ WebApplication1/Models/EmployeeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            IList<Employee> employees = new List<Employee>();
+            employees.Add(new Employee() { ID = 1234, FirstName = "Hemanth", LastName = "Kumar" });
+            employees.Add(new Employee() { ID = 123, FirstName = "Surya", LastName = "c" });
+            employees.Add(new Employee() { ID = 123, FirstName = "Rohit", LastName = "c" });
+
+            ViewData["employees"] = employees;
+
+            return View();
+        }
+    }
+}
